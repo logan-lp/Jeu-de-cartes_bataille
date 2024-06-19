62ae5878701363908c31bdb26f3b5da44eefa643
Logan <57043298+logan-lp@users.noreply.github.com>
2024-06-19 18:09:22 +0200

    Update bataille.py

--- bataille.py
+++ bataille.py
@@ -56,9 +56,11 @@ def afficher_vainqueur_tour(gagnant, nom1, nom2) :
 
 def afficher_vainqueur_jeu(gagnant, jeu1, jeu2, nom1, nom2) :
     if gagnant == 'bataille':
-        if jeu1 == []:
+        #if jeu1 == []:
+        if len(jeu_joueur1) == 1:
             print(nom2, "a gagné le jeu.")
-        elif jeu2 == []:
+        #elif jeu2 == []:
+        elif len(jeu_joueur1) == 1:
             print(nom1, "a gagné le jeu.")
     if gagnant == 'joueur1':
         print(nom1, "a gagné le jeu.")
@@ -83,6 +85,7 @@ def valeur_carte(carte:str) :
         case "Va":
             return 11
         case _:
+            #print(int(c))
             return int(c)
 
 
@@ -120,37 +123,64 @@ def ordonner_jeu(jeu_vainqueur:list, jeu_perdant:list) :#2 cartes a la fin du pa
     jeu_vainqueur.remove(jeu_vainqueur[0])
     jeu_perdant.remove(jeu_perdant[0])
 
-def gestion_bataille(jeu_joueur1:list, jeu_joueur2:list, gagnant_bataille:str) :
-    global lst_bataille
-    lst_bataille = []
-    if gagnant_bataille=='bataille':
-        a=1
-
-    for i in range(0,1):
+def gestion_bataille(jeu_joueur1:list, jeu_joueur2:list, lst_bataille) :
+    #global lst_bataille
+    #lst_bataille = []
+    #for i in range(1):
+        #if jeu_joueur1==[] or jeu_joueur2==[]:
+        #    break
         #print(i)
+    if jeu_joueur1==[] or jeu_joueur2==[]:
+        return ""
+
+    lst_bataille.append(jeu_joueur1[0])
+    lst_bataille.append(jeu_joueur2[0])
+    jeu_joueur1.remove(jeu_joueur1[0])
+    jeu_joueur2.remove(jeu_joueur2[0])
+    if jeu_joueur1==[] or jeu_joueur2==[]:
+        return ""
+
+    if len(jeu_joueur1) !=1:
         lst_bataille.append(jeu_joueur1[1])
-        lst_bataille.append(jeu_joueur2[1])
         jeu_joueur1.remove(jeu_joueur1[1])
+    if len(jeu_joueur2)!=1:
+        lst_bataille.append(jeu_joueur2[1])
         jeu_joueur2.remove(jeu_joueur2[1])
-    if comparer_carte(jeu_joueur1[0], jeu_joueur2[0]) == 'bataille':
-        gestion_bataille(jeu_joueur1, jeu_joueur2, gagnant_bataille)
-    elif comparer_carte(jeu_joueur1[0], jeu_joueur2[0]) == 'joueur1':
-        jeu_joueur1.append(lst_bataille)
-        lst_bataille = []
-    else:
-        jeu_joueur2.append(lst_bataille)
-        lst_bataille = []
+
+        if comparer_carte(jeu_joueur1[0], jeu_joueur2[0]) == 'bataille':
+            gestion_bataille(jeu_joueur1, jeu_joueur2, lst_bataille)
+        elif comparer_carte(jeu_joueur1[0], jeu_joueur2[0]) == 'joueur1':
+            for i in lst_bataille:
+                jeu_joueur1.append(i)
+            lst_bataille = []
+        else:
+            for i in lst_bataille:
+                jeu_joueur2.append(i)
+            lst_bataille = []
+
+    if len(jeu_joueur1) == 1:
+        print("")
+        afficher_vainqueur_jeu('joueur2', jeu_joueur1, jeu_joueur2, nom_joueur1, nom_joueur2)
+    if len(jeu_joueur2) == 1:
+        print("")
+        afficher_vainqueur_jeu('joueur1', jeu_joueur1, jeu_joueur2, nom_joueur1, nom_joueur2)
+
+
+
+
 
 
 
 def gestion_tour(gagnant:str, jeu_joueur1:list, jeu_joueur2:list) :
+    if jeu_joueur1==[] or jeu_joueur2==[]:
+        return ""
     if gagnant== 'joueur1':
         ordonner_jeu(jeu_joueur1, jeu_joueur2)
-    else:
-        perdant = jeu_joueur1
+    elif gagnant== 'joueur2':
+        #perdant = jeu_joueur1
         ordonner_jeu(jeu_joueur2, jeu_joueur1)
-    #if gagnant == 'bataille':
-        #gestion_bataille(jeu_joueur1, jeu_joueur2, 'bataille')
+    if gagnant == 'bataille':
+        gestion_bataille(jeu_joueur1, jeu_joueur2, [])
 
 
 def triBulles(lst):
@@ -173,13 +203,17 @@ def jeu() :
     pass
 
 jeu_complet = jeu_de_cartes()
+global nom_joueur1
+global nom_joueur2
 nom_joueur1 = 'Logan' #input("Tapez le nom du premier joueur : ")
 nom_joueur2 = 'Elena' #input("Tapez le nom du deuxième joueur : ")
 jeu_joueur1 = distribution_jeu(jeu_complet, 26)
 jeu_joueur2 = distribution_jeu(jeu_complet, 26)
 
-while jeu_joueur1 != [] or jeu_joueur1 != []:
+while jeu_joueur1 != [] or jeu_joueur2 != []:
     #chercher vainqueur du tour ???
+    if jeu_joueur1==[] or jeu_joueur2==[]:
+        break
     afficher_nombres_cartes(jeu_joueur1, jeu_joueur2, nom_joueur1, nom_joueur2)
     afficher_carte(jeu_joueur1, jeu_joueur2)
     gagnant = comparer_carte(jeu_joueur1[0], jeu_joueur2[0])
@@ -193,55 +227,3 @@ if jeu_joueur1 == []:
 else:
     gagnant = 'joueur1'
 afficher_vainqueur_jeu(gagnant, jeu_joueur1, jeu_joueur2, nom_joueur1, nom_joueur2)
-
-
-
-
-#print(jeu_joueur1)
-#print(jeu_joueur2)
-#print(comparer_carte(jeu_joueur1[0], jeu_joueur2[0]))
-#gestion_tour(jeu_joueur1, jeu_joueur2, comparer_carte(jeu_joueur1[0], jeu_joueur2[0]))
-
-
-
-
-
-#afficher_carte(jeu_joueur1, jeu_joueur2)
-
-
-#shuffle(jeu_temporaire) #Mélange le paquet
-#print(jeu_temporaire)
-
-
-#jeu_joueur1 = ['As ♠', '5 ♥', '2 ♠']
-#jeu_joueur3 = ['Dame ♠', '7 ♦', '8 ♦', 'As ♣', 'Valet ♣']
-#lst1 = []
-#lst2 = []
-#for i in jeu_joueur1:
- #   print(valeur_carte(i))
-
-
-
-
-
-
-
-
-
-
-#for i in range (0,len(jeu_joueur2)):
-#    print("-", valeur_carte(jeu_joueur2[i]))
-#    print("^", valeur_carte(jeu_joueur2[i-1]))
-#    if valeur_carte(jeu_joueur2[i]) <= valeur_carte(jeu_joueur2[i-1]):
-#        lst2.append(jeu_joueur2[i])
-#        del(jeu_joueur2[i])
-        #jeu_joueur2.append(jeu_joueur2[i])
-        #jeu_joueur2.remove(jeu_joueur2[i])
-
-
-
-        #jeu_joueur1.remove(i)
-    #if valeur_carte(i) <= valeur_carte(i):
-    #    lst1.append(i)
-#print(lst2)
-#print(jeu_joueur2)
